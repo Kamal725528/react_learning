39590d501b295e5cc3de7a1ca507e40f5f07ffd2
kamal725528 <kamalsingh725528@gmail.com>
2023-11-08 00:08:56 +0530

    hooks_project

---
+++ counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [count, setCount]=useState(15);
+
+  const add=()=>{
+    setCount(count+1);
+  }
+
+  const remove=()=>{
+    setCount(count-1);
+  }
+
+
+  return (
+    <>
+     <h1>counter project || Kamal </h1>
+    <h2>counter value : {count} </h2>
+    <br></br>
+    <button onClick={add}>add counter {count}</button>
+    <br></br>
+    <button onClick={remove}>remove counter {count}</button>
+    <p>footer: {count}</p>
+    </>
+  )
+}
+
+export default App
