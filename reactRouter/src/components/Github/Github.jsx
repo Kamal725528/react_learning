0bc729950fadfc4d8bd0d1cbb98620373acb15cd
kamal725528 <kamalsingh725528@gmail.com>
2023-11-18 00:50:28 +0530

    react_routing

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+ export default function Github(){
+   
+    const data = useLoaderData();
+
+    // const [data, setData]= useState([]);
+    
+    // useEffect(()=>{
+    //  fetch('https://api.github.com/users/Kamal725528')
+    // .then(response=>response.json())
+    // .then(data=>setData(data))
+    // },[]);                        // use loader one
+
+    return(
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Repos: {data.public_repos}
+        <img src={data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+// or use Loader() to enhance Ux by fast loading
+
+ export const githubInfoLoader= async()=>{
+
+    const response= await fetch('https://api.github.com/users/Kamal725528')
+    return  response.json();
+}
+
+
+
+
+
